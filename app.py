64b308754e025d532b11cd60e96b42a241a18cd0
Udolf15 <hashmi.masif@gmail.com>
2020-03-06 01:25:48 +0530

    removing unwanted import with more comments

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template , request ,flash , redirect, url_for, session, logging
+from flask import Flask, render_template, redirect
 import configFileGenerator
 from src import dataFetcher
 
@@ -7,15 +7,22 @@ app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 data_fetched = None
 
+
+# This route redirect to login page
 @app.route("/")
 def home():
     return redirect("http://127.0.0.1:5000/login")
 
 
+# Login Route
 @app.route('/login' , methods= ['GET','POST'])
 def login():
 
     CONFIG_FILE_GENERATOR = configFileGenerator.ConfigFileGenerator()
+
+    # If request is a POST method then use the information to fetch data from the XNAT instance
+    # Update the global data_fetched list and redirect to the dashboard 
+    # where the global data_fetched will be used
 
     if(request.method== 'POST'):
         userDetail = request.form
@@ -30,12 +37,19 @@ def login():
         return render_template('login.html')
 
 
+# Dashboard route
 @app.route('/dashboard')
 def dashboard():
     
     global data_fetched
-    print(data_fetched)
-    return render_template('dashboard.html',data_fetched = data_fetched)
+
+    # If the data_fetched is None result in error in login credentials
+
+    if(data_fetched == None):
+        print(data_fetched)
+        return render_template('dashboard.html',data_fetched = data_fetched)
+    else:
+        return "Please login again with correct credentials no data found "
 
 
 
