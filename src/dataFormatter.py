ba8a188f1a2bf620470bb1dbc580f7b52465a04a
Udolf15 <hashmi.masif@gmail.com>
2020-03-06 10:01:31 +0530

    remove the extra methods can be used  in future for table

--- src/dataFormatter.py
+++ src/dataFormatter.py
@@ -4,70 +4,6 @@ class Formatter:
 
     data = dataFetcher.Fetcher()   # Data Fetcher Object
 
-    def projects_formatter(self):
-
-        # Show the important fields of the project
-
-        projects_table = self.data.get_projects()
-
-        if(projects_table != None):
-            
-            print("PROJECT DETAILS")
-            print("Number of project visible are ",len(projects_table))
-            print("\n\n\n\n")
-
-            # Looping through each project to show the important fields
-
-            for project in projects_table:
-
-                print("Project Creation Date : ", project['insert_date'])
-                print("Project Owner Name: ", project['insert_user'])
-                print("Project Name : ", project['name'])
-                print("Project Description : ", project['description'])
-                print("Project Users : ", project['project_users'].split('<br/>'))
-                print("Project Visibility : ", project['project_access']) 
-                print("Project Members : ", project['project_members'].split('<br/>'))
-                print("Project Collaborators : ", project['project_collabs'].split('<br/>'))
-                print("\n\n\n\n")
-
-        else:
-            print("Please check network connection")
-            print("Please check username or password in your configuration file")
-
-
-    def subjects_formatter(self): 
-
-        # Show the the important fields of subjects
-
-        subjects_table = self.data.get_subjects()
-
-        if(subjects_table != None):
-            
-            print("SUBJECT DETAILS")
-            print("Number of subjects visible are ", len(subjects_table))
-            print("\n\n\n\n")
-
-            # Looping through each subject to show the important fields
-
-            for subject in subjects_table:
-
-                print("Subject Creation Date : ", subject['insert_date'])
-                print("Subject Creator Name: ", subject['insert_user'])
-                print("Subject associated with ",subject['project'],"project")
-                print("Subject Gender ", subject['gender_text'])
-                print("Subject Handedness : ", subject['handedness_text'])
-                print("Subject Date of Birth : ", subject['dob'])
-                print("Subject Education : ", subject['educ']) 
-                print("Subject SES : ", subject['ses'])
-                print("Subject Race : ", subject['race'])
-                print("Subject Ethnicity : ", subject['ethnicity'])
-                print("\n\n\n\n")
-
-        else:
-            print("Please check network connection")
-            print("Please check username or password in your configuration file")
-
-
     def stats(self):
 
         # Show the overall data counts present on XNAT 
@@ -108,9 +44,15 @@ class Formatter:
 
             # Making a list to be returned to the flask app for jinja
 
-            return [len(projects_table), len(subjects_table), len(experiments_table), 
-                    counter_left_hand, counter_right_hand, counter_unknown, 
-                    counter_male, counter_female, counter_unknown]
+            return [len(projects_table), 
+                    len(subjects_table), 
+                    len(experiments_table), 
+                    counter_left_hand, 
+                    counter_right_hand,
+                    counter_unknown, 
+                    counter_male, 
+                    counter_female, 
+                    counter_unknown ]
         else:
             print("Please check network connection")
             print("Please check username or password in configuration file")
