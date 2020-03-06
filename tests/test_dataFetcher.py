883f41bbfcfefc9d6af5787dd38c1c465d2ed3a8
Udolf15 <hashmi.masif@gmail.com>
2020-03-06 10:23:32 +0530

    create tests for dataFetcher.py

---
+++ tests/test_dataFetcher.py
@@ -0,0 +1,31 @@
+import pytest
+from src import dataFetcher, check_network
+
+data_fetcher = dataFetcher.Fetcher()    # Object of the Fetcher class
+
+
+def test_get_projects():
+
+    out = data_fetcher.get_projects()
+    if(check_network.connect()):
+        assert str(type(out)) == "<class 'pyxnat.core.jsonutil.JsonTable'>"
+    else:
+        assert out == None
+
+
+def test_get_subjects():
+
+    out = data_fetcher.get_subjects()
+    if(check_network.connect()):
+        assert str(type(out)) == "<class 'pyxnat.core.jsonutil.JsonTable'>"
+    else:
+        assert out == None
+
+
+def test_get_experiments():
+
+    out = data_fetcher.get_experiments()
+    if(check_network.connect()):
+        assert str(type(out)) == "<class 'pyxnat.core.jsonutil.JsonTable'>"
+    else:
+        assert out == None
